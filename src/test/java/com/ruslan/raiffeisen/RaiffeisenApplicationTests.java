c7cc00ff2d7678592a89b2651eeb5a0ddccc843b
Ruslan Khaliullin <ruslan_Khaliullin@epam.com>
2021-10-11 15:01:24 +0300

    Initial commit

---
+++ src/test/java/com/ruslan/raiffeisen/RaiffeisenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslan.raiffeisen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiffeisenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
